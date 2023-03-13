f9eb15c06a6cb8fd13d8f1186a71022069ba94ce
Yu Yang <yangyu2010@aliyun.com>
2023-03-13 15:29:24 +0800

    Initial Commit

---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by Yu Yang on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
