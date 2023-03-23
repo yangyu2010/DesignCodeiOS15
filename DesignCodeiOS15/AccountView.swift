d44b029e6232504af696280d0abded838c242ec9
Yu Yang <yangyu2010@aliyun.com>
2023-03-23 18:33:00 +0800

    导航 链接

---
+++ DesignCodeiOS15/AccountView.swift
@@ -0,0 +1,88 @@
+//
+//  AccountView.swift
+//  DesignCodeiOS15
+//
+//  Created by Yu Yang on 3/23/23.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                VStack {
+                    Image(systemName: "person.crop.circle.badge.checkmark")
+                        .symbolVariant(.circle.fill)
+                        .font(.system(size: 32))
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.blue, .blue.opacity(0.3))
+                        .padding()
+                        .background(Circle().fill(.ultraThinMaterial))
+                        .background(
+                            Image(systemName: "hexagon")
+                                .symbolVariant(.fill)
+                                .foregroundStyle(.blue)
+                                .font(.system(size: 200))
+                                .offset(x: -50, y: -110)
+                        )
+                    Text("Yu Yang")
+                        .font(.title.weight(.semibold))
+                    HStack {
+                        Image(systemName: "location")
+                            .imageScale(.small)
+                        Text("China")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                    
+                Section {
+                    NavigationLink(destination: ContentView()) {
+                        Label("Hello, World!", systemImage: "gear")
+                    }
+                    NavigationLink {
+                        Text("Billing")
+                    } label: {
+                        Label("Billing", systemImage: "creditcard")
+                    }
+                    NavigationLink(destination: ContentView()) {
+                        Label("Help", systemImage: "questionmark")
+                    }
+                }
+//                .accentColor(.red)
+                .listRowSeparatorTint(.red)
+                .listRowSeparator(.hidden)
+                
+                Section {
+                    Link(destination: URL(string: "https://apple.com")!) {
+                        HStack {
+                            Label("Apple", systemImage: "apple.logo")
+                            Spacer()
+                            Image(systemName: "link")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    Link(destination: URL(string: "https://youtube.com")!) {
+                        HStack {
+                            Label("Youtube", systemImage: "tv")
+                            Spacer()
+                            Image(systemName: "link")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
+                .accentColor(.primary)
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Account")
+        }
+    }
+}
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
+}
