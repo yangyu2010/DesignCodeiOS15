2238fa72b3872b5380954388065eed5b87401aa0
Yu Yang <yangyu2010@aliyun.com>
2023-04-03 11:33:40 +0800

    抽取本地组件 P11

--- DesignCodeiOS15/AccountView.swift
+++ DesignCodeiOS15/AccountView.swift
@@ -9,92 +9,113 @@ import SwiftUI
 
 struct AccountView: View {
     @State var isDelete = false
+    @State var isPinned = false
     
     var body: some View {
         NavigationView {
             List {
-                VStack {
-                    Image(systemName: "person.crop.circle.badge.checkmark")
-                        .symbolVariant(.circle.fill)
-                        .font(.system(size: 32))
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.blue, .blue.opacity(0.3))
-                        .padding()
-                        .background(Circle().fill(.ultraThinMaterial))
-                        .background(
-                            Image(systemName: "hexagon")
-                                .symbolVariant(.fill)
-                                .foregroundStyle(.blue)
-                                .font(.system(size: 200))
-                                .offset(x: -50, y: -110)
-                        )
-                    Text("Yu Yang")
-                        .font(.title.weight(.semibold))
-                    HStack {
-                        Image(systemName: "location")
-                            .imageScale(.small)
-                        Text("China")
-                            .foregroundColor(.secondary)
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                .padding()
-                    
-                Section {
-                    NavigationLink(destination: ContentView()) {
-                        Label("Hello, World!", systemImage: "gear")
-                    }
-                    NavigationLink {
-                        Text("Billing")
-                    } label: {
-                        Label("Billing", systemImage: "creditcard")
-                    }
-                    NavigationLink(destination: ContentView()) {
-                        Label("Help", systemImage: "questionmark")
-                    }
-                }
-//                .accentColor(.red)
-                .listRowSeparatorTint(.red)
-                .listRowSeparator(.hidden)
-                
-                Section {
-                    if !isDelete {
-                        Link(destination: URL(string: "https://apple.com")!) {
-                            HStack {
-                                Label("Apple", systemImage: "apple.logo")
-                                Spacer()
-                                Image(systemName: "link")
-                                    .foregroundColor(.secondary)
-                            }
-                        }
-                        .swipeActions(edge: .leading, allowsFullSwipe: false) {
-                            Button {
-                                 isDelete = true
-                            } label: {
-                                Label("Delete", systemImage: "trash")
-                            }
-                            .tint(.red)
-                            
-                            Button(action: { }) {
-                                Label("Pin", systemImage: "pin")
-                            }
-                            .tint(.yellow)
-                        }
-                    }
-                    Link(destination: URL(string: "https://youtube.com")!) {
-                        HStack {
-                            Label("Youtube", systemImage: "tv")
-                            Spacer()
-                            Image(systemName: "link")
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                }
-                .accentColor(.primary)
+                profile
+                menu
+                links
             }
             .listStyle(.insetGrouped)
             .navigationTitle("Account")
         }
+    }
+    
+    var profile: some View {
+        VStack {
+            Image(systemName: "person.crop.circle.badge.checkmark")
+                .symbolVariant(.circle.fill)
+                .font(.system(size: 32))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.blue, .blue.opacity(0.3))
+                .padding()
+                .background(Circle().fill(.ultraThinMaterial))
+                .background(
+                    Image(systemName: "hexagon")
+                        .symbolVariant(.fill)
+                        .foregroundStyle(.blue)
+                        .font(.system(size: 200))
+                        .offset(x: -50, y: -110)
+                )
+            Text("Yu Yang")
+                .font(.title.weight(.semibold))
+            HStack {
+                Image(systemName: "location")
+                    .imageScale(.small)
+                Text("China")
+                    .foregroundColor(.secondary)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+    }
+    
+    var menu: some View {
+        Section {
+            NavigationLink(destination: ContentView()) {
+                Label("Hello, World!", systemImage: "gear")
+            }
+            NavigationLink {
+                Text("Billing")
+            } label: {
+                Label("Billing", systemImage: "creditcard")
+            }
+            NavigationLink(destination: ContentView()) {
+                Label("Help", systemImage: "questionmark")
+            }
+        }
+//                .accentColor(.red)
+        .listRowSeparatorTint(.red)
+        .listRowSeparator(.hidden)
+    }
+    
+    var links: some View {
+        Section {
+            if !isDelete {
+                Link(destination: URL(string: "https://apple.com")!) {
+                    HStack {
+                        Label("Apple", systemImage: "apple.logo")
+                        Spacer()
+                        Image(systemName: "link")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .swipeActions(edge: .leading, allowsFullSwipe: false) {
+                    Button {
+                         isDelete = true
+                    } label: {
+                        Label("Delete", systemImage: "trash")
+                    }
+                    .tint(.red)
+                    pinButton
+                }
+            }
+            Link(destination: URL(string: "https://youtube.com")!) {
+                HStack {
+                    Label("Youtube", systemImage: "tv")
+                    Spacer()
+                    Image(systemName: "link")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .swipeActions {
+                pinButton
+            }
+        }
+        .accentColor(.primary)
+    }
+    
+    var pinButton: some View {
+        Button(action: { isPinned.toggle() }) {
+            if isPinned {
+                Label("UnPin", systemImage: "pin.slash")
+            } else {
+                Label("Pin", systemImage: "pin")
+            }
+        }
+        .tint(isPinned ? .gray : .yellow)
     }
 }
 
