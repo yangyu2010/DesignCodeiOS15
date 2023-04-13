0c74aa5c19376c537e3f54b578f8081de7b88419
Yu Yang <yangyu2010@aliyun.com>
2023-04-13 16:01:06 +0800

    P18 - Tab Bar Selection

--- DesignCodeiOS15/Tab.swift
+++ DesignCodeiOS15/Tab.swift
@@ -7,15 +7,23 @@
 
 import SwiftUI
 
-struct Tab: Identifiable {
+struct TabItem: Identifiable {
     var id = UUID()
     var text: String
     var icon: String
+    var tab: Tab
 }
 
 var tabItems = [
-    Tab(text: "Learn Now", icon: "house"),
-    Tab(text: "Explore", icon: "magnifyingglass"),
-    Tab(text: "Notifications", icon: "bell"),
-    Tab(text: "Library", icon: "rectangle.stack")
+    TabItem(text: "Learn Now", icon: "house", tab: .home),
+    TabItem(text: "Explore", icon: "magnifyingglass", tab: .explore),
+    TabItem(text: "Notifications", icon: "bell", tab: .notifications),
+    TabItem(text: "Library", icon: "rectangle.stack", tab: .library)
 ]
+
+enum Tab: String {
+    case home
+    case explore
+    case notifications
+    case library
+}
--- DesignCodeiOS15/TabBar.swift
+++ DesignCodeiOS15/TabBar.swift
@@ -8,25 +8,43 @@
 import SwiftUI
 
 struct TabBar: View {
+    @State private var selectedTab: Tab = .home
+    
     var body: some View {
         ZStack(alignment: .bottom) {
-            ContentView()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .offset(y: 300)
-            HStack {
-                ForEach(tabItems) { item in
-                    VStack(spacing: 0) {
-                        Image(systemName: item.icon)
-                            .symbolVariant(.fill)
-                            .font(.body.bold())
-                            .frame(width: 44, height: 29)
-                        Text(item.text)
-                            .font(.caption2)
-                            .lineLimit(1)
-                    }
-                    .frame(maxWidth: .infinity)
+            Group {
+                switch selectedTab {
+                case .home:
+                    ContentView()
+                case .explore:
+                    AccountView()
+                case .notifications:
+                    AccountView()
+                case .library:
+                    AccountView()
                 }
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            HStack {
+                ForEach(tabItems) { item in
+                    Button {
+                        selectedTab = item.tab
+                    } label: {
+                        VStack(spacing: 0) {
+                            Image(systemName: item.icon)
+                                .symbolVariant(.fill)
+                                .font(.body.bold())
+                                .frame(width: 44, height: 29)
+                            Text(item.text)
+                                .font(.caption2)
+                                .lineLimit(1)
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
+                    .foregroundColor(selectedTab == item.tab ? .primary : .secondary)
+                }
+            } 
             .padding(.horizontal, 8)
             .padding(.top, 14)
             .frame(height: 88, alignment: .top)
