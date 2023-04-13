f31c26e2bdaf322480f61d9c6243e82ee2f5753b
Yu Yang <yangyu2010@aliyun.com>
2023-04-13 17:21:06 +0800

    P19 - Tab Bar Animation

--- DesignCodeiOS15/TabBar.swift
+++ DesignCodeiOS15/TabBar.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct TabBar: View {
     @State private var selectedTab: Tab = .home
+    @State private var color = Color.teal
     
     var body: some View {
         ZStack(alignment: .bottom) {
@@ -29,7 +30,10 @@ struct TabBar: View {
             HStack {
                 ForEach(tabItems) { item in
                     Button {
-                        selectedTab = item.tab
+                        withAnimation(.spring(response: 0.3, dampingFraction: 0.7)) {
+                            selectedTab = item.tab
+                            color = item.color
+                        }
                     } label: {
                         VStack(spacing: 0) {
                             Image(systemName: item.icon)
@@ -49,7 +53,49 @@ struct TabBar: View {
             .padding(.top, 14)
             .frame(height: 88, alignment: .top)
             .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
-            .strokeStyle(cornerRadius: 34)
+            .background(
+                HStack {
+                    if selectedTab == .library {
+                        Spacer()
+                    }
+                    if selectedTab == .explore {
+                        Spacer()
+                    }
+                    if selectedTab == .notifications {
+                        Spacer()
+                        Spacer()
+                    }
+                    Circle().fill(color).frame(width: 80)
+                    if selectedTab == .home {
+                        Spacer()
+                    }
+                    if selectedTab == .explore {
+                        Spacer()
+                        Spacer()
+                    }
+                    if selectedTab == .notifications {
+                        Spacer()
+                    }
+
+                    
+//                    if selectedTab == .home {
+//                        Circle().fill(Color.red).frame(width: 80)
+//                    }
+//                    Spacer()
+//                    if selectedTab == .explore {
+//                        Circle().fill(Color.red).frame(width: 80)
+//                    }
+//                    Spacer()
+//                    if selectedTab == .notifications {
+//                        Circle().fill(Color.red).frame(width: 80)
+//                    }
+//                    Spacer()
+//                    if selectedTab == .library {
+//                        Circle().fill(Color.red).frame(width: 80)
+//                    }
+                }
+            )
+//            .strokeStyle(cornerRadius: 34)
             .frame(maxHeight: .infinity, alignment: .bottom)
             .ignoresSafeArea()
         }
